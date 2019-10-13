7aa0c0c75c3c6c379678820de7fccfed43ecc383
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-10-13 19:49:30 +0300

    change the image sources

--- src/Pokecard/Pokecard.js
+++ src/Pokecard/Pokecard.js
@@ -2,14 +2,23 @@ import React, {Component} from 'react';
 import './Pokecard.css';
 
 export class Pokecard extends Component {
-
+handleId(id) {
+    
+   if(Math.floor(id / 10)=== 0){
+        return `00${id}`;
+   }else if(Math.floor(id / 100) === 0){
+       return `0${id}`
+   }
+   return id
+}
   render() {
     const {name, id, type, base_experience} = this.props
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-name">{name}</h1>
         <img
-          src={` https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}/>
+          src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${this.handleId(id)}.png`}/>
+       {console.log(this.handleId(id))}
         <p>Type: {type}</p>
         <p>EXP: {base_experience}</p>
       </div>
