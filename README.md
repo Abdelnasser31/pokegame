9069044b36fb56d458f5f4789aa9fd634ee512b0
Abdelnasser <abdelnasser.kh10@gmail.com>
2019-10-22 00:46:55 +0300

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-This project was bootstrapped with [Create React App]
-This porject is for practicing React following the React Course by Colt Steele On Udemy.
-
-
-## Objectives
-
-Practice Props
-
---
+++ precache-manifest.36d7ecf6b3f2bdf1329fa2daccc5206b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293f5a9404c6d90c04d3b7b3fcbabfb5",
+    "url": "/pokegame-react/index.html"
+  },
+  {
+    "revision": "dacd109bf15558d5c404",
+    "url": "/pokegame-react/static/css/main.6b9eef46.chunk.css"
+  },
+  {
+    "revision": "65642c68988b97c555f5",
+    "url": "/pokegame-react/static/js/2.ae1f98d4.chunk.js"
+  },
+  {
+    "revision": "dacd109bf15558d5c404",
+    "url": "/pokegame-react/static/js/main.698fe6a3.chunk.js"
+  },
+  {
+    "revision": "4693bbf8e1c89a787d82",
+    "url": "/pokegame-react/static/js/runtime-main.8d967c71.js"
+  }
+]);
